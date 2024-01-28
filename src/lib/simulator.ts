0be98258dd240ce621b64150e338c01ad5b961e0
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-01-28 20:40:04 +0800

    feat(sim): draft simulator function that takes in some initial state and updates it

--- src/lib/simulator.ts
+++ src/lib/simulator.ts
@@ -1,3 +1,24 @@
-export function* simulator() {
-	for (let i = 0; i < 10; i++) yield i;
+export interface Car {
+	x: number;
+	vx: number;
+}
+
+export interface State {
+	cars: Array<Car>;
+	step: number;
+}
+
+export function* simulator(init: State) {
+	const state = structuredClone(init);
+
+	while (true) {
+		// Update cars by adding vx
+		state.cars.map((car) => (car.x += car.vx));
+
+		// Temporary halting condition
+		if (state.cars.some((car) => car.x >= 100)) return;
+
+		state.step++;
+		yield state;
+	}
 }
