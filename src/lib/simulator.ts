c50391e3420df7a04de902f68c029b2c029fb6d5
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-02-01 23:21:46 +0800

    feat(sim): basic car updates and precompute dw

--- src/lib/simulator.ts
+++ src/lib/simulator.ts
@@ -1,24 +1,61 @@
+import type { AlgorithmInputs, Algorithm } from './algorithms';
+import type { ParameterInput, SimParameterInput } from './stores/parameter/types';
+
 export interface Car {
 	x: number;
 	vx: number;
+	ax: number;
+	abr: number;
 }
 
 export interface State {
-	cars: Array<Car>;
+	FV: Car;
+	LV: Car;
 	tick: number;
+	dw: number;
 }
 
-export function* simulator(init: State) {
-	const state = structuredClone(init);
+function adapter(FV: Car, LV: Car, Sim: SimParameterInput): AlgorithmInputs {
+	return {
+		vf: FV.vx,
+		af: FV.abr,
+		vl: LV.vx,
+		al: LV.abr,
+		tr: Sim.tr,
+		ts: Sim.ts,
+		dmin: Sim.dmin
+	};
+}
+
+export function* simulator(params: ParameterInput, fcwa: Algorithm) {
+	const FV: Car = {
+		x: 0,
+		vx: params.FV.vx,
+		ax: 0.001,
+		abr: params.FV.abr
+	};
+
+	const LV: Car = {
+		x: params.Sim.dmin,
+		vx: params.LV.vx,
+		ax: 0,
+		abr: params.LV.abr
+	};
+
+	const state: State = { FV, LV, tick: 0, dw: 0 };
+
+	state.dw = fcwa(adapter(FV, LV, params.Sim));
 
 	while (true) {
-		// Update cars by adding vx
-		state.cars.map((car) => (car.x += car.vx));
+		state.tick += 1;
 
-		// Temporary halting condition
-		if (state.cars.some((car) => car.x >= 100)) return;
+		state.FV.x += state.FV.vx;
+		state.FV.vx += state.FV.ax;
 
-		state.tick++;
+		state.LV.x += state.LV.vx;
+		state.LV.vx += state.LV.ax;
+
+		if (state.FV.x > state.LV.x) return;
 		yield state;
 	}
 }
