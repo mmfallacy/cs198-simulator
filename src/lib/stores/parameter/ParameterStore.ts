17c20cea25ba491f087059f71f3abab3f71bfb4d
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-02-01 22:38:23 +0800

    feat(params): Parameter Store

---
+++ src/lib/stores/parameter/ParameterStore.ts
@@ -0,0 +1,21 @@
+import { writable } from 'svelte/store';
+import type { ParameterInput } from './types';
+
+const initialParameters: ParameterInput = {
+	FV: {
+		vx: 0,
+		abr: 0
+	},
+	LV: {
+		vx: 0,
+		abr: 0
+	},
+	Sim: {
+		tr: 0,
+		ts: 0,
+		dmin: 0,
+		N: 0
+	}
+} as const;
+
+export const ParameterStore = writable(initialParameters);
