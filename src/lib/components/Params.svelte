32ef66003b6738f052c080080dc0341e4b0314b5
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-04-14 18:08:39 +0800

    fix: initial distance runs into string concatenation issues as svelte does not store input as number

--- src/lib/components/Params.svelte
+++ src/lib/components/Params.svelte
@@ -4,26 +4,26 @@
 
 <h2>Following Vehicle (FV):</h2>
 <h4>Initial Velocity (+ km/h)</h4>
-<input bind:value={$params.FV.vx} />
+<input type="number" bind:value={$params.FV.vx} />
 <h4>Acceleration due to braking (- mps^2)</h4>
-<input bind:value={$params.FV.abr} />
+<input type="number" bind:value={$params.FV.abr} />
 <h4>Acceleration (+ mps^2)</h4>
-<input bind:value={$params.FV.ax} />
+<input type="number" bind:value={$params.FV.ax} />
 
 <h2>Leading Vehicle (LV):</h2>
 <h4>Initial Velocity (+ km/h)</h4>
-<input bind:value={$params.LV.vx} />
+<input type="number" bind:value={$params.LV.vx} />
 <!-- <h4>Acceleration due to braking (- km/h^2)</h4>
-<input bind:value={$params.LV.abr} /> -->
+<input type="number" bind:value={$params.LV.abr} /> -->
 
 <h2>Simulation Parameters:</h2>
 <!-- <h4>Reaction time delay (ms)</h4>
-<input bind:value={$params.Sim.tr} />
+<input type="number" bind:value={$params.Sim.tr} />
 <h4>System time delay (ms)</h4>
-<input bind:value={$params.Sim.ts} /> -->
+<input type="number" bind:value={$params.Sim.ts} /> -->
 <h4>Minimum safety distance (m)</h4>
-<input bind:value={$params.Sim.dmin} />
+<input type="number" bind:value={$params.Sim.dmin} />
 <h4>Initial distance between FV and LV</h4>
-<input bind:value={$params.Sim.id} />
+<input type="number" bind:value={$params.Sim.id} />
 <!-- <h4>Number of test runs</h4>
-<input bind:value={$params.Sim.N} /> -->
+<input type="number" bind:value={$params.Sim.N} /> -->
