ee7905b5a44592a0f7aeae57da594c39832cbc82
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-05-21 00:56:01 +0800

    chore: remove console log in quadrealroot

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,5 @@
 export function quadrealroot(a: number, b: number, c: number) {
 	const disc = Math.sqrt(b * b - 4 * a * c);
-	console.log('disc', disc);
 	if (isNaN(disc)) return [NaN];
 
 	return [(-b + disc) / (2 * a), (-b - disc) / (2 * a)];
