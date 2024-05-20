eceba3dcf0f20465da74c0a2a157a7e346fdecd1
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-05-21 01:00:26 +0800

    feat: async parallel runner

---
+++ src/routes/parallel/+page.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	import type { ParameterInput } from '$lib/stores/parameter/types';
+	import { simulator } from '$lib/simulator';
+	import type { Algorithm } from '$lib/algorithms';
+	import { Algorithms, CAR_DIMENSIONS, MAX_TICK, RATIO } from '$lib/const';
+	import { initialParameters } from '$lib/stores/parameter/ParameterStore';
+
+	type RunnerParams = {
+		maxDistanceInMeters?: number;
+		maxDistanceInPx?: number;
+		maxSeconds?: number;
+		maxTicks?: number;
+	};
+
+	function isUndefined(value: unknown): value is undefined {
+		return typeof value === 'undefined';
+	}
+
+	async function runner(
+		simParams: ParameterInput,
+		fcwa: Algorithm,
+		params: RunnerParams = { maxTicks: MAX_TICK }
+	) {
+		const sim = simulator(simParams, fcwa);
+
+		const { maxDistanceInMeters, maxDistanceInPx, maxSeconds, maxTicks } = params;
+
+		while (true) {
+			const { value, done } = sim.next();
+
+			if (done) throw new Error('Detected Collision.');
+
+			const distance = value.LV.x + CAR_DIMENSIONS.w / RATIO.px_per_m;
+
+			if (!isUndefined(maxDistanceInMeters) && distance >= maxDistanceInMeters) return value;
+
+			if (!isUndefined(maxDistanceInPx) && distance * RATIO.px_per_m >= maxDistanceInPx)
+				return value;
+
+			if (!isUndefined(maxSeconds) && value.tick / simParams.Sim.tps >= maxSeconds) return value;
+
+			if (!isUndefined(maxTicks) && value.tick >= maxTicks) return value;
+		}
+	}
+
+	async function start() {
+		const results = await Promise.allSettled([
+			runner(initialParameters, Algorithms['honda'], { maxDistanceInMeters: 500 }),
+			runner(initialParameters, Algorithms['hirstgraham'], { maxDistanceInMeters: 500 }),
+			runner(initialParameters, Algorithms['bellarusso'], { maxDistanceInMeters: 500 }),
+
+			runner(initialParameters, Algorithms['honda'], { maxDistanceInPx: 720 }),
+			runner(initialParameters, Algorithms['hirstgraham'], { maxDistanceInPx: 720 }),
+			runner(initialParameters, Algorithms['bellarusso'], { maxDistanceInPx: 720 }),
+
+			runner(initialParameters, Algorithms['honda'], { maxTicks: 500 }),
+			runner(initialParameters, Algorithms['hirstgraham'], { maxTicks: 500 }),
+			runner(initialParameters, Algorithms['bellarusso'], { maxTicks: 500 }),
+
+			runner(initialParameters, Algorithms['honda'], { maxSeconds: 5 }),
+			runner(initialParameters, Algorithms['hirstgraham'], { maxSeconds: 5 }),
+			runner(initialParameters, Algorithms['bellarusso'], { maxSeconds: 5 })
+		]);
+
+		console.log(results);
+	}
+</script>
+
+<button on:click={start}> Start </button>
