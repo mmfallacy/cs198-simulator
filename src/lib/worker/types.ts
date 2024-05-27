245b190ffd77e7ff64f9ffa0345c17591eb7097c
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-05-27 23:17:52 +0800

    feat: worker input and return types

---
+++ src/lib/worker/types.ts
@@ -0,0 +1,27 @@
+import { StateSchema } from '$lib/simulator/types';
+import { Algorithms } from '$lib/const';
+import { ParameterSchema } from '$lib/stores/parameter/types';
+import * as v from 'valibot';
+import type { DeepReadonly } from '$lib/utils';
+
+export const RunnerParamsSchema = v.object({
+	maxDistanceInMeters: v.optional(v.number()),
+	maxDistanceInPx: v.optional(v.number()),
+	maxElapsedTimeInSeconds: v.optional(v.number()),
+	maxSeconds: v.optional(v.number()),
+	maxTicks: v.optional(v.number())
+});
+
+export type RunnerParams = v.Input<typeof RunnerParamsSchema>;
+
+export const WorkerActionSchema = v.object({
+	simParams: ParameterSchema,
+	fcwaKey: v.picklist(Object.keys(Algorithms) as Array<keyof typeof Algorithms>),
+	params: RunnerParamsSchema
+});
+
+export type WorkerAction = v.Input<typeof WorkerActionSchema>;
+
+export const WorkerReturnSchema = StateSchema;
+
+export type WorkerReturn = DeepReadonly<v.Input<typeof WorkerReturnSchema>>;
