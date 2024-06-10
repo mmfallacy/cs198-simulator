52e986ccc9af0a5e0260dda84d8a689a1f37519c
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-06-10 23:13:22 +0800

    feat: export database as json

--- src/routes/database/+page.svelte
+++ src/routes/database/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { db } from '$lib/database/database';
 	import { liveQuery } from 'dexie';
+	import { exportDB } from 'dexie-export-import';
 
 	let runs = get();
 	$: runs = get();
@@ -9,13 +10,20 @@
 	const PAGE_SIZE = 25;
 
 	function get() {
-		return liveQuery(async () => {
-			const collection = db.runs;
-			return (await collection.offset(offset).limit(PAGE_SIZE).toArray()).map((result) => {
-				result.params = JSON.parse(result.params);
-				return result;
-			});
-		});
+		// return liveQuery(async () => {
+		// 	const collection = db.runs;
+		// 	return await collection.offset(offset).limit(PAGE_SIZE).toArray();
+		// });
+	}
+
+	async function exp() {
+		const blob = await exportDB(db);
+		const url = URL.createObjectURL(blob);
+		const a = document.createElement('a');
+		a.href = url;
+		a.download = 'export.json';
+		a.click();
+		URL.revokeObjectURL(url);
 	}
 
 	$: console.log($runs);
@@ -28,3 +36,4 @@
 		{/each}
 	{/each}
 {/if}
+<button on:click={exp}>Export DB</button>
