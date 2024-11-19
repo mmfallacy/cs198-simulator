b0378e329a170d7bf2c2dfccc2c8a37168ea338d
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-11-19 21:42:55 +0800

    feat: ask for filename

--- src/routes/database/+page.svelte
+++ src/routes/database/+page.svelte
@@ -1,39 +1,19 @@
 <script lang="ts">
 	import { db } from '$lib/database/database';
-	import { liveQuery } from 'dexie';
 	import { exportDB } from 'dexie-export-import';
-
-	let runs = get();
-	$: runs = get();
-	let offset = 0;
-
-	const PAGE_SIZE = 25;
-
-	function get() {
-		// return liveQuery(async () => {
-		// 	const collection = db.runs;
-		// 	return await collection.offset(offset).limit(PAGE_SIZE).toArray();
-		// });
-	}
 
 	async function exp() {
 		const blob = await exportDB(db);
 		const url = URL.createObjectURL(blob);
 		const a = document.createElement('a');
 		a.href = url;
-		a.download = 'export.json';
+		a.download = `${filename}.json`;
 		a.click();
 		URL.revokeObjectURL(url);
 	}
 
-	$: console.log($runs);
+	let filename: string = 'export';
 </script>
 
-{#if $runs}
-	{#each $runs as run (run.params)}
-		{#each Object.entries(run) as [key, value]}
-			{key}: {value}
-		{/each}
-	{/each}
-{/if}
+<input type="text" bind:value={filename} placeholder="place filename here" />
 <button on:click={exp}>Export DB</button>
