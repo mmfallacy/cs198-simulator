803f4f836d02977b01d26e50591330783d70ee9c
mmfallacy <31348500+mmfallacy@users.noreply.github.com>
2024-12-15 19:42:57 +0800

    feat: list all tasks for parallel

--- src/routes/parallel/+page.svelte
+++ src/routes/parallel/+page.svelte
@@ -6,22 +6,42 @@
 	import { Algorithms } from '$lib/const';
 	import { adapter, quadrealroot } from '$lib/utils';
 	import type { ParameterInput } from '$lib/stores/parameter/types';
-
-	const tasks = new Array<WorkerAction>();
-
-	const defaultTask: WorkerAction = {
-		simParams: initialParameters,
-		fcwaKey: 'honda',
-		params: {
-			maxDistanceInMeters: 1000
-		}
-	};
-
-	defaultTask.simParams.Sim.tps = 120;
+	import { assert } from '$lib/assert';
 
 	const XLIM = 10,
 		YLIM = 30,
 		GRAN = 300;
+
+	const dAspace = linspace(-XLIM, XLIM, GRAN),
+		dVspace = linspace(-YLIM, YLIM, GRAN),
+		vfspace = [3, 12, 24];
+
+	type ValidAlgorithm = keyof typeof Algorithms;
+
+	const algorithms: ValidAlgorithm[] = ['honda', 'hirstgraham', 'bellarusso', 'onecar'];
+
+	const tasks = $state(
+		algorithms.flatMap((fcwa) =>
+			vfspace.map((vf) => {
+				return {
+					fcwa,
+					vf,
+					run: true
+				};
+			})
+		)
+	);
+
+	const defaultTask: WorkerAction = {
+		simParams: initialParameters,
+		fcwaKey: 'onecar',
+		params: {
+			maxDistanceInMeters: 1000,
+			maxTicks: 20000
+		}
+	};
+
+	defaultTask.simParams.Sim.tps = 120;
 
 	// Derived from numpy's `linspace` function
 	// https://github.com/numpy/numpy/blob/v1.26.0/numpy/core/function_base.py#L24-L182
@@ -52,50 +72,76 @@
 	let totalDispatched = 0,
 		totalDone = 0;
 
-	function start() {
+	function start(fcwa: ValidAlgorithm, vf: number) {
 		// for (const time of [15, 5, 15, 2, 3]) {
 		// 	const clone = structuredClone(defaultTask);
 		// 	clone.params.maxElapsedTimeInSeconds = time;
 
 		// 	workers.dispatch(clone).then(console.log);
 		// }
-		for (const vf of [36]) //, 6, 12, 18, 36])
-			for (const dA of linspace(-XLIM, XLIM, GRAN))
-				for (const dV of linspace(-YLIM, YLIM, GRAN)) {
-					const clone = structuredClone(defaultTask);
-					clone.simParams.FV.vx = vf;
-					clone.simParams.LV.vx = vf - dV;
-					clone.simParams.FV.ax = dA;
-					clone.simParams.Sim.id = Algorithms[clone.fcwaKey](adapter(clone.simParams));
-					clone.simParams.Sim.algo = clone.fcwaKey;
+		for (const dA of dAspace)
+			for (const dV of dVspace) {
+				const clone = structuredClone(defaultTask);
+				clone.simParams.FV.vx = vf;
+				clone.simParams.LV.vx = vf - dV;
+				clone.simParams.FV.ax = dA;
+				clone.simParams.Sim.algo = fcwa;
+				clone.simParams.Sim.id = Algorithms[fcwa](adapter(clone.simParams));
 
-					if (dV > vf || clone.simParams.Sim.id < 0 || mttc(clone.simParams) <= 0) {
-						// console.warn('Skipping');
-						continue;
-					}
-
-					totalDispatched += 1;
-
-					workers.dispatch(clone).then((res) => {
-						console.log(res);
-						totalDone += 1;
-					});
+				if (dV > vf || clone.simParams.Sim.id < 0 || mttc(clone.simParams) <= 0) {
+					// console.warn('Skipping');
+					continue;
 				}
+
+				totalDispatched += 1;
+
+				workers.dispatch(clone).then((res) => {
+					console.log(res);
+					totalDone += 1;
+				});
+			}
 		allDispatched = true;
+	}
+	$inspect(tasks);
+
+	function updateTask(fcwa: string, vf: number, run: boolean) {
+		const match = tasks.find((el) => el.fcwa == fcwa && el.vf == vf);
+		assert(typeof match !== 'undefined');
+		match.run = run;
 	}
 
 	let allDispatched = false;
 </script>
 
 Results:
-<div class="flex flex-col gap-2">
-	<button on:click={start}> Start </button>
-	<h1>Total Dispatched: {totalDispatched}</h1>
-	<h1>Total Done: {totalDone}</h1>
-	{#each Object.entries($display) as [id, worker]}
-		<h3>Worker #{id}: {worker.status}</h3>
-	{/each}
-	{#if allDispatched}
-		<h1>All tasks dispatched! Please wait for remaining workers to finish</h1>
-	{/if}
+<div class="m-4 grid grid-cols-[max-content] items-center gap-2">
+	<h1>Worker Pool:</h1>
+	<section class="grid w-auto grid-cols-20 gap-2">
+		{#each Object.entries($display) as [id, worker] (id)}
+			<div class="flex flex-col items-center justify-evenly rounded-sm bg-slate-200 px-2 py-1">
+				<span class="font-bold">{id}</span>
+				<span>{worker.status === 'idle' ? '⌛' : '✅'}</span>
+			</div>
+		{/each}
+	</section>
+	<h1>Tasks:</h1>
+	<ul class=" ">
+		{#each tasks as task (task)}
+			{@const { fcwa, vf, run } = task}
+			<li class="flex justify-between gap-3">
+				<h3>{fcwa} (vf={vf})</h3>
+				<span class="*:ml-2">
+					<input
+						type="checkbox"
+						onchange={function () {
+							assert(typeof this.checked === 'boolean');
+							updateTask(fcwa, vf, this.checked);
+						}}
+						checked={run}
+					/>
+					<button onclick={() => start(fcwa, vf)} class="px-3 py-2"> <h3>Start</h3> </button>
+				</span>
+			</li>
+		{/each}
+	</ul>
 </div>
